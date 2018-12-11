7d976d3463451aa9d13ae4407ced16b20644b5ec
Ernest Fraiman <upapaus@gmail.com>
2018-12-11 11:57:31 +0000

    created start code

---
+++ client/src/views/stock_form_view.js
@@ -0,0 +1,14 @@
+const StockFormView = function (container, symbol) {
+  this.container = container;
+};
+
+StockFormView.prototype.render = function () {
+  const buttonsContainer = document.createElement('div');
+  buttonsContainer.className = 'buttons-container';
+
+
+
+
+};
+
+module.exports = StockFormView;
